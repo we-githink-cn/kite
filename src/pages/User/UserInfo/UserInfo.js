891d1a6c8f206eb422395f9195d9ec0b0d87d734
549595297@qq.com <jia327926>
2019-04-08 18:26:29 +0800

    作者信息

--- src/pages/User/UserInfo/UserInfo.js
+++ src/pages/User/UserInfo/UserInfo.js
@@ -24,11 +24,267 @@ export default class UserInfo extends Component {
 
   render() {
     let index = Math.floor(Math.random() * 5);
-    console.log(index);
     let UserHeader = null;
-    switch (0) {
+    let isBack = true;
+
+    let UserContent = (<div className={styles.UserHeaderInfo}>
+      <div className={styles.UserHeaderInfoAvatar}>
+        <Ant.Avatar
+          shape="square"
+          size={84}
+          src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png"
+        />
+      </div>
+      <div className={styles.UserHeaderUserInfo}>
+        <div className={styles.UserHeaderUserName}>
+          <div className={styles.UserHeaderUserNameContent}>
+            <h2>
+              <a href="" style={{ lineHeight: '44px' }}>
+                Githinkcn
+              </a>
+            </h2>
+            <span className={styles.UserHeaderUserId}>549595297</span>
+          </div>
+          <div className={styles.UserHeaderUserNameContentAction}>
+            <Button color="green" compact>
+              私信
+            </Button>
+            <Button color="orange" compact>
+              关注
+            </Button>
+          </div>
+        </div>
+        <div className={styles.UserHeaderOtherInfo}>
+          <Ant.Tooltip placement="top" title={true ? '站长' : '社区管理员'}>
+            <Icon
+              name="user secret"
+              color={true ? 'red' : 'green'}
+              style={{ lineHeight: '20px' }}
+            />
+          </Ant.Tooltip>
+
+          <div className={styles.userVip}>
+            <Popup trigger={<img src={svip6} className={styles.userVipImg} />} size="mini">
+              <div>
+                <span className={styles.userExpName}>黑钻会员：</span>
+                <div className={styles.userExpBar}>
+                  <div
+                    className={styles.userExpNumber}
+                    style={{
+                      backgroundColor: '#4CAF50',
+                      width: (20000 / 20000) * 100 + '%',
+                    }}
+                  >
+                    20000/20000
+                  </div>
+                </div>
+              </div>
+            </Popup>
+          </div>
+          <div>
+            <span className={styles.userDesc}>大梦初醒，荒唐了一生。</span>
+          </div>
+        </div>
+        <div className={styles.UserHeaderAction}>
+          <div className={styles.UserActionItem}>
+            <Ant.Tooltip placement="top" title="新手/第 8 号会员">
+              <Icon name="user" />
+            </Ant.Tooltip>
+          </div>
+          <div className={styles.UserActionItem}>
+            <Ant.Tooltip placement="top" title="粉丝 109.8k">
+              <Icon name="chess queen" />
+            </Ant.Tooltip>
+          </div>
+          <div className={styles.UserActionItem}>
+            <Ant.Tooltip placement="top" title="关注 9">
+              <Icon name="heart" />
+            </Ant.Tooltip>
+          </div>
+          <div className={styles.UserActionItem}>
+            <Ant.Tooltip placement="top" title="收获点赞 109.8k">
+              <Icon name="thumbs up" />
+            </Ant.Tooltip>
+          </div>
+          <div className={styles.UserActionItem}>
+            <Ant.Tooltip placement="top" title="文章 22 篇">
+              <Icon name="book" />
+            </Ant.Tooltip>
+          </div>
+          <div className={styles.UserActionItem}>
+            <Ant.Tooltip placement="top" title="济南">
+              <Icon name="location arrow" />
+            </Ant.Tooltip>
+          </div>
+          <div className={styles.UserActionItem}>
+            <a href="https://blog.githink.cn" target="_blank">
+              <Ant.Tooltip placement="top" title="https://blog.githink.cn">
+                <Icon name="linkify" />
+              </Ant.Tooltip>
+            </a>
+          </div>
+          <div className={styles.UserActionItem}>
+            <a href="https://github.com/githinkcn" target="_blank">
+              <Ant.Tooltip placement="top" title="https://github.com/githinkcn">
+                <Icon name="github" />
+              </Ant.Tooltip>
+            </a>
+          </div>
+          <div className={styles.UserActionItem}>
+            <Ant.Tooltip placement="top" title="549595297">
+              <Icon name="qq" />
+            </Ant.Tooltip>
+          </div>
+          <div className={styles.UserActionItem}>
+            <a href="https://weibo.com/549595297" target="_blank">
+              <Ant.Tooltip placement="top" title="549595297">
+                <Icon name="weibo" />
+              </Ant.Tooltip>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>);
+    let UserBgContent = (<div
+      className={styles.UserBgHeader}
+      style={{
+        backgroundImage: `url(https://img.hacpai.com/bg/home/1353745196354_1551340749271.jpg?imageslim)`,
+      }}
+    >
+      <div
+        className={styles.UserBgHeaderContent}
+        style={{
+          backgroundImage:
+            'linear-gradient(90deg, #FF6D185c, rgb(255, 255, 255, 0.76), #FF6D185c)',
+        }}
+      >
+        <div className={styles.UserBgHeaderInfo}>
+          <div
+            className={styles.UserBgHeaderAvatar}
+            style={{
+              boxShadow: 'rgba(255, 109, 24, 0.68) 0px 0px 1px',
+              backgroundImage: `url("https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png")`,
+            }}
+          />
+          <div className={styles.UserHeaderBgUserInfo}>
+            <div className={styles.UserHeaderUserName}>
+              <div className={styles.UserHeaderUserNameContent}>
+                <h2>
+                  <a href="" style={{ lineHeight: '44px' }}>
+                    Githinkcn
+                  </a>
+                </h2>
+                <span className={styles.UserHeaderUserId}>549595297</span>
+              </div>
+              <div className={styles.UserHeaderUserNameContentAction}>
+                <Button color="green" compact>
+                  私信
+                </Button>
+                <Button color="orange" compact>
+                  关注
+                </Button>
+              </div>
+            </div>
+            <div className={styles.UserHeaderOtherInfo}>
+              <Ant.Tooltip placement="top" title={true ? '站长' : '社区管理员'}>
+                <Icon
+                  name="user secret"
+                  color={true ? 'red' : 'green'}
+                  style={{ lineHeight: '20px' }}
+                />
+              </Ant.Tooltip>
+
+              <div className={styles.userVip}>
+                <Popup
+                  trigger={<img src={svip6} className={styles.userVipImg} />}
+                  size="mini"
+                >
+                  <div>
+                    <span className={styles.userExpName}>黑钻会员：</span>
+                    <div className={styles.userExpBar}>
+                      <div
+                        className={styles.userExpNumber}
+                        style={{
+                          backgroundColor: '#4CAF50',
+                          width: (20000 / 20000) * 100 + '%',
+                        }}
+                      >
+                        20000/20000
+                      </div>
+                    </div>
+                  </div>
+                </Popup>
+              </div>
+              <div>
+                <span className={styles.userDesc}>大梦初醒，荒唐了一生。</span>
+              </div>
+            </div>
+            <div className={styles.UserHeaderAction}>
+              <div className={styles.UserActionItem}>
+                <Ant.Tooltip placement="top" title="新手/第 8 号会员">
+                  <Icon name="user" />
+                </Ant.Tooltip>
+              </div>
+              <div className={styles.UserActionItem}>
+                <Ant.Tooltip placement="top" title="粉丝 109.8k">
+                  <Icon name="chess queen" />
+                </Ant.Tooltip>
+              </div>
+              <div className={styles.UserActionItem}>
+                <Ant.Tooltip placement="top" title="关注 9">
+                  <Icon name="heart" />
+                </Ant.Tooltip>
+              </div>
+              <div className={styles.UserActionItem}>
+                <Ant.Tooltip placement="top" title="收获点赞 109.8k">
+                  <Icon name="thumbs up" />
+                </Ant.Tooltip>
+              </div>
+              <div className={styles.UserActionItem}>
+                <Ant.Tooltip placement="top" title="文章 22 篇">
+                  <Icon name="book" />
+                </Ant.Tooltip>
+              </div>
+              <div className={styles.UserActionItem}>
+                <Ant.Tooltip placement="top" title="济南">
+                  <Icon name="location arrow" />
+                </Ant.Tooltip>
+              </div>
+              <div className={styles.UserActionItem}>
+                <a href="https://blog.githink.cn" target="_blank">
+                  <Ant.Tooltip placement="top" title="https://blog.githink.cn">
+                    <Icon name="linkify" />
+                  </Ant.Tooltip>
+                </a>
+              </div>
+              <div className={styles.UserActionItem}>
+                <a href="https://github.com/githinkcn" target="_blank">
+                  <Ant.Tooltip placement="top" title="https://github.com/githinkcn">
+                    <Icon name="github" />
+                  </Ant.Tooltip>
+                </a>
+              </div>
+              <div className={styles.UserActionItem}>
+                <Ant.Tooltip placement="top" title="549595297">
+                  <Icon name="qq" />
+                </Ant.Tooltip>
+              </div>
+              <div className={styles.UserActionItem}>
+                <a href="https://weibo.com/549595297" target="_blank">
+                  <Ant.Tooltip placement="top" title="549595297">
+                    <Icon name="weibo" />
+                  </Ant.Tooltip>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>);
+
+    switch (index) {
       case 0:
-        UserHeader = (
+        UserHeader = !isBack ? (
           <div
             className={styles.UserHeader}
             style={{
@@ -36,180 +292,85 @@ export default class UserInfo extends Component {
                 'linear-gradient(90deg, rgba(131, 72, 187, 0.36), rgba(255, 255, 255, 0.76), rgba(131, 72, 187, 0.36))',
             }}
           >
-            <div className={styles.UserHeaderInfo}>
-              <div className={styles.UserHeaderInfoAvatar}>
-                <Ant.Avatar
-                  shape="square"
-                  size={84}
-                  src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png"
-                />
-              </div>
-              <div className={styles.UserHeaderUserInfo}>
-                <div className={styles.UserHeaderUserName}>
-                  <div className={styles.UserHeaderUserNameContent}>
-                    <h2>
-                      <a href="" style={{ lineHeight: '44px' }}>
-                        Githinkcn
-                      </a>
-                    </h2>
-                    <span className={styles.UserHeaderUserId}>549595297</span>
-                  </div>
-                  <div className={styles.UserHeaderUserNameContentAction}>
-                    <Button color="green" compact>
-                      私信
-                    </Button>
-                    <Button color="orange" compact>
-                      关注
-                    </Button>
-                  </div>
-                </div>
-                <div className={styles.UserHeaderOtherInfo}>
-                  <Ant.Tooltip placement="top" title={true ? '站长' : '社区管理员'}>
-                    <Icon
-                      name="user secret"
-                      color={true ? 'red' : 'green'}
-                      style={{ lineHeight: '20px' }}
-                    />
-                  </Ant.Tooltip>
-
-                  <div className={styles.userVip}>
-                    <Popup trigger={<img src={svip6} className={styles.userVipImg} />} size="mini">
-                      <div>
-                        <span className={styles.userExpName}>黑钻会员：</span>
-                        <div className={styles.userExpBar}>
-                          <div
-                            className={styles.userExpNumber}
-                            style={{
-                              backgroundColor: '#4CAF50',
-                              width: (20000 / 20000) * 100 + '%',
-                            }}
-                          >
-                            20000/20000
-                          </div>
-                        </div>
-                      </div>
-                    </Popup>
-                  </div>
-                  <div>
-                    <span className={styles.userDesc}>大梦初醒，荒唐了一生。</span>
-                  </div>
-                </div>
-                <div className={styles.UserHeaderAction}>
-                  <div className={styles.UserActionItem}>
-                    <Ant.Tooltip placement="top" title="新手/第 8 号会员">
-                      <Icon name="user" />
-                    </Ant.Tooltip>
-                  </div>
-                  <div className={styles.UserActionItem}>
-                    <Ant.Tooltip placement="top" title="粉丝 109.8k">
-                      <Icon name="chess queen" />
-                    </Ant.Tooltip>
-                  </div>
-                  <div className={styles.UserActionItem}>
-                    <Ant.Tooltip placement="top" title="关注 9">
-                      <Icon name="heart" />
-                    </Ant.Tooltip>
-                  </div>
-                  <div className={styles.UserActionItem}>
-                    <Ant.Tooltip placement="top" title="收获点赞 109.8k">
-                      <Icon name="thumbs up" />
-                    </Ant.Tooltip>
-                  </div>
-                  <div className={styles.UserActionItem}>
-                    <Ant.Tooltip placement="top" title="文章 22 篇">
-                      <Icon name="book" />
-                    </Ant.Tooltip>
-                  </div>
-                  <div className={styles.UserActionItem}>
-                    <Ant.Tooltip placement="top" title="济南">
-                      <Icon name="location arrow" />
-                    </Ant.Tooltip>
-                  </div>
-                  <div className={styles.UserActionItem}>
-                    <a href="https://blog.githink.cn" target="_blank">
-                      <Ant.Tooltip placement="top" title="https://blog.githink.cn">
-                        <Icon name="linkify" />
-                      </Ant.Tooltip>
-                    </a>
-                  </div>
-                  <div className={styles.UserActionItem}>
-                    <a href="https://github.com/githinkcn" target="_blank">
-                      <Ant.Tooltip placement="top" title="https://github.com/githinkcn">
-                        <Icon name="github" />
-                      </Ant.Tooltip>
-                    </a>
-                  </div>
-                  <div className={styles.UserActionItem}>
-                    <Ant.Tooltip placement="top" title="549595297">
-                      <Icon name="qq" />
-                    </Ant.Tooltip>
-                  </div>
-                  <div className={styles.UserActionItem}>
-                    <a href="https://weibo.com/549595297" target="_blank">
-                      <Ant.Tooltip placement="top" title="549595297">
-                        <Icon name="weibo" />
-                      </Ant.Tooltip>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
+            {UserContent}
           </div>
+        ) : (
+          UserBgContent
         );
         break;
       case 1:
-        UserHeader = (
+        UserHeader = !isBack ?(
           <div
             className={styles.UserHeader}
             style={{
               backgroundImage:
                 'linear-gradient(90deg, rgba(118, 199, 129, 0.36), rgba(255, 255, 255, 0.76), rgba(118, 199, 129, 0.36))',
             }}
-          />
+          >
+            {UserContent}
+          </div>
+        ): (
+          UserBgContent
         );
         break;
       case 2:
-        UserHeader = (
+        UserHeader = !isBack ?(
           <div
             className={styles.UserHeader}
             style={{
               backgroundImage:
                 'linear-gradient(90deg, rgba(152, 130, 134, 0.36), rgba(255, 255, 255, 0.76), rgba(152, 130, 134, 0.36))',
             }}
-          />
+          >
+            {UserContent}
+          </div>
+        ): (
+          UserBgContent
         );
         break;
       case 3:
-        UserHeader = (
+        UserHeader = !isBack ?(
           <div
             className={styles.UserHeader}
             style={{
               backgroundImage:
                 'linear-gradient(90deg, rgba(7, 152, 145, 0.36), rgba(255, 255, 255, 0.76), rgba(7, 152, 145, 0.36))',
             }}
-          />
+          >
+            {UserContent}
+          </div>
+        ): (
+          UserBgContent
         );
         break;
       case 4:
-        UserHeader = (
+        UserHeader = !isBack ?(
           <div
             className={styles.UserHeader}
             style={{
               backgroundImage:
                 'linear-gradient(90deg, rgba(144, 130, 67, 0.36), rgba(255, 255, 255, 0.76), rgba(144, 130, 67, 0.36))',
             }}
-          />
+          >
+            {UserContent}
+          </div>
+        ): (
+          UserBgContent
         );
         break;
       case 5:
-        UserHeader = (
+        UserHeader = !isBack ?(
           <div
             className={styles.UserHeader}
             style={{
               backgroundImage:
                 'linear-gradient(90deg, rgba(181, 134, 103, 0.36), rgba(255, 255, 255, 0.76), rgba(181, 134, 103, 0.36))',
             }}
-          />
+          >
+            {UserContent}
+          </div>
+        ): (
+          UserBgContent
         );
         break;
       default:
