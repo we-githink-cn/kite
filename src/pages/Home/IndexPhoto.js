f6f4ef42f9c7131ab117f2ae579576003db8da79
Maozk <549595297@qq.com>
2019-04-19 23:15:43 +0800

    图圈详细页面

--- src/pages/Home/IndexPhoto.js
+++ src/pages/Home/IndexPhoto.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Icon, Modal, Button, Dropdown, Checkbox} from 'semantic-ui-react';
+import { Icon, Modal, Button, Dropdown, Checkbox } from 'semantic-ui-react';
 import * as Ant from 'antd';
 import Vditor from 'vditor';
 import router from 'umi/router';
@@ -7,7 +7,7 @@ import router from 'umi/router';
 import PhotoItem from '../../components/Photo/PhotoItem';
 
 import styles from './IndexPhoto.less';
-import RecCard from "../../components/Index/RecCard";
+import RecCard from '../../components/Index/RecCard';
 const options = [
   { key: 'English', text: 'English', value: 'English' },
   { key: 'French', text: 'French', value: 'French' },
@@ -17,27 +17,27 @@ const options = [
 ];
 
 export default class Index extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       vditor: null,
       putVisible: false,
-      previewVisible:false,
+      detailVisible: false,
+      previewVisible: false,
       previewImage: '',
-      fileList:[],
+      fileList: [],
       tags: options,
       currentTags: [],
-    }
+    };
   }
 
-  componentWillMount () {}
+  componentWillMount() {}
 
-  componentDidMount () {}
+  componentDidMount() {}
 
-  componentWillUpdate (nextProps, nextState) {}
+  componentWillUpdate(nextProps, nextState) {}
 
-  componentDidUpdate (prevProps, prevState) {}
+  componentDidUpdate(prevProps, prevState) {}
 
   _initVditor = data => {
     return new Vditor(data.id, {
@@ -51,111 +51,131 @@ export default class Index extends Component {
         },
       },
       height: data.height,
-      counter: 4096,
+      counter: 512,
       resize: {
         position: 'bottom',
         enable: data.resize,
       },
       placeholder: data.placeholder,
-      toolbar: [{
-        hotkey: "⌘-e",
-        name: "emoji",
-        tipPosition: "ne",
-      }, {
-        hotkey: "⌘-h",
-        name: "headings",
-        tipPosition: "ne",
-      }, {
-        hotkey: "⌘-b",
-        name: "bold",
-        prefix: "**",
-        suffix: "**",
-        tipPosition: "ne",
-      }, {
-        hotkey: "⌘-i",
-        name: "italic",
-        prefix: "*",
-        suffix: "*",
-        tipPosition: "ne",
-      }, {
-        hotkey: "⌘-s",
-        name: "strike",
-        prefix: "~~",
-        suffix: "~~",
-        tipPosition: "ne",
-      }, {
-        hotkey: "⌘-d",
-        name: "line",
-        prefix: "---\n",
-        tipPosition: "n",
-      }, {
-        hotkey: "⌘-.",
-        name: "quote",
-        prefix: "> ",
-        tipPosition: "n",
-      }, {
-        hotkey: "⌘-l",
-        name: "list",
-        prefix: "* ",
-        tipPosition: "n",
-      }, {
-        hotkey: "⌘-o",
-        name: "ordered-list",
-        prefix: "1. ",
-        tipPosition: "n",
-      }, {
-        hotkey: "⌘-j",
-        name: "check",
-        prefix: "* [ ] ",
-        tipPosition: "n",
-      },{
-        hotkey: "⌘-z",
-        name: "undo",
-        tipPosition: "n",
-      }, {
-        hotkey: "⌘-y",
-        name: "redo",
-        tipPosition: "n",
-      },{
-        hotkey: "⌘-k",
-        name: "link",
-        prefix: "[",
-        suffix: "](https://)",
-        tipPosition: "n",
-      }, {
-        hotkey: "⌘-p",
-        name: "preview",
-        tipPosition: "nw",
-      }, {
-        hotkey: "⌘-'",
-        name: "fullscreen",
-        tipPosition: "nw",
-      }, {
-        name: "br",
-      }],
+      toolbar: [
+        {
+          hotkey: '⌘-e',
+          name: 'emoji',
+          tipPosition: 'ne',
+        },
+        {
+          hotkey: '⌘-h',
+          name: 'headings',
+          tipPosition: 'ne',
+        },
+        {
+          hotkey: '⌘-b',
+          name: 'bold',
+          prefix: '**',
+          suffix: '**',
+          tipPosition: 'ne',
+        },
+        {
+          hotkey: '⌘-i',
+          name: 'italic',
+          prefix: '*',
+          suffix: '*',
+          tipPosition: 'ne',
+        },
+        {
+          hotkey: '⌘-s',
+          name: 'strike',
+          prefix: '~~',
+          suffix: '~~',
+          tipPosition: 'ne',
+        },
+        {
+          hotkey: '⌘-d',
+          name: 'line',
+          prefix: '---\n',
+          tipPosition: 'n',
+        },
+        {
+          hotkey: '⌘-.',
+          name: 'quote',
+          prefix: '> ',
+          tipPosition: 'n',
+        },
+        {
+          hotkey: '⌘-l',
+          name: 'list',
+          prefix: '* ',
+          tipPosition: 'n',
+        },
+        {
+          hotkey: '⌘-o',
+          name: 'ordered-list',
+          prefix: '1. ',
+          tipPosition: 'n',
+        },
+        {
+          hotkey: '⌘-j',
+          name: 'check',
+          prefix: '* [ ] ',
+          tipPosition: 'n',
+        },
+        {
+          hotkey: '⌘-z',
+          name: 'undo',
+          tipPosition: 'n',
+        },
+        {
+          hotkey: '⌘-y',
+          name: 'redo',
+          tipPosition: 'n',
+        },
+        {
+          hotkey: '⌘-k',
+          name: 'link',
+          prefix: '[',
+          suffix: '](https://)',
+          tipPosition: 'n',
+        },
+        {
+          hotkey: '⌘-p',
+          name: 'preview',
+          tipPosition: 'nw',
+        },
+        {
+          hotkey: "⌘-'",
+          name: 'fullscreen',
+          tipPosition: 'nw',
+        },
+        {
+          name: 'br',
+        },
+      ],
     });
   };
 
-  handlePutOpenClick =()=>{
-    this.setState({
-      putVisible: true
-    },()=>{
-      const vditor = this._initVditor({
-        id: 'put-content',
-        show: false,
-        height: 200,
-        placeholder: '请输入内容',
-        resize: true,
-      });
-      this.setState({
-        vditor: vditor,
-      });
-    })
+  handlePutOpenClick = () => {
+    this.setState(
+      {
+        putVisible: true,
+      },
+      () => {
+        const vditor = this._initVditor({
+          id: 'put-content',
+          show: false,
+          height: 200,
+          placeholder: '请输入内容',
+          resize: true,
+        });
+        this.setState({
+          vditor: vditor,
+        });
+      }
+    );
   };
 
   handleCancel = () => this.setState({ previewVisible: false });
 
-  handlePreview = (file) => {
+  handlePreview = file => {
     this.setState({
       previewImage: file.url || file.thumbUrl,
       previewVisible: true,
@@ -172,8 +192,21 @@ export default class Index extends Component {
 
   handleTagChange = (e, { value }) => this.setState({ currentTags: value });
 
+  handleOpenDetail = () => {
+    this.setState({
+      detailVisible: true,
+    });
+  };
+
   render() {
-    const {putVisible, previewVisible, previewImage, fileList, currentTags} =this.state;
+    const {
+      putVisible,
+      previewVisible,
+      detailVisible,
+      previewImage,
+      fileList,
+      currentTags,
+    } = this.state;
     const uploadButton = (
       <div>
         <Ant.Icon type="plus" />
@@ -200,13 +233,13 @@ export default class Index extends Component {
         avatar: 'https://react.semantic-ui.com/images/avatar/small/jenny.jpg',
       },
     ];
-    return(
+    return (
       <div className={styles.PhotoWrapper}>
         <div className={styles.PhotoMainWrapper}>
           <div className={styles.PhotoMainContent}>
-            <PhotoItem src='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'/>
-            <PhotoItem src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'/>
-            <PhotoItem src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg'/>
+            <PhotoItem src="https://react.semantic-ui.com/images/avatar/large/elliot.jpg" />
+            <PhotoItem src="https://react.semantic-ui.com/images/avatar/large/steve.jpg" />
+            <PhotoItem src="https://react.semantic-ui.com/images/avatar/large/daniel.jpg" />
           </div>
         </div>
         <div className={styles.PhotoMateWrapper}>
@@ -224,7 +257,7 @@ export default class Index extends Component {
               </div>
             </div>
           </div>
-          <div style={{marginTop: '20px'}}>
+          <div style={{ marginTop: '20px' }}>
             <div className="GlobalLineHeader">
               <h4>
                 <Icon name="star" color="teal" /> 推荐作者
@@ -248,20 +281,45 @@ export default class Index extends Component {
             </div>
           </div>
         </div>
-        <Modal size='small' open={putVisible} closeIcon onClose={()=>{this.setState({putVisible: false})}}>
+        <Modal
+          size="small"
+          open={putVisible}
+          closeIcon
+          onClose={() => {
+            this.setState({ putVisible: false });
+          }}
+        >
+          <div className={styles.moduleHeader} style={{ paddingTop: '10px' }}>
+            <h3>发图</h3>
+          </div>
           <Modal.Content>
             <div className={styles.PutBody}>
-              <Ant.Upload
-                action=""
-                listType="picture-card"
-                fileList={fileList}
-                onPreview={this.handlePreview}
-                onChange={this.handleChange}
-              >
-                {fileList.length >= 9 ? null : uploadButton}
-              </Ant.Upload>
+              <div style={{ width: '100%' }} className="Put-Upload">
+                <Ant.Upload.Dragger
+                  name="file"
+                  multiple={true}
+                  action=""
+                  listType="picture-card"
+                  fileList={fileList}
+                  onPreview={this.handlePreview}
+                  onChange={this.handleChange}
+                >
+                  {fileList.length >= 9 ? (
+                    <p className="ant-upload-text">最多支持上传9张图片。</p>
+                  ) : (
+                    <div>
+                      <p className="ant-upload-drag-icon">
+                        <Ant.Icon type="inbox" />
+                      </p>
+                      <p className="ant-upload-text">
+                        单击或拖动文件到此区域进行上传，支持单个或批量上传。
+                      </p>
+                    </div>
+                  )}
+                </Ant.Upload.Dragger>
+              </div>
 
-              <div id="put-content" style={{ width:'678px', margin: '0 auto' }}/>
+              <div id="put-content" style={{ width: '678px', margin: '0 auto' }} />
 
               <div className={styles.wrapper}>
                 <div className={styles.weFlex}>
@@ -303,26 +361,52 @@ export default class Index extends Component {
                     <Icon size="large" name="hand point right outline" />{' '}
                     <span className={styles.ftH5}>发文</span>
                     <span className={styles.ftGray}>
-                  <span className={styles.ftGreen}>图圈</span>分享你身边的美好
-                </span>
+                      <span className={styles.ftGreen}>图圈</span>分享你身边的美好
+                    </span>
                   </div>
                   <Button.Group size="small">
-                    <Button onClick={this.handleSettingClick}>设置</Button>
+                    <Button
+                      onClick={() => {
+                        this.setState({ putVisible: false });
+                      }}
+                    >
+                      取消
+                    </Button>
                     <Button.Or text="or" style={{ marginBottom: '2px' }} />
-                    <Button positive onClick={this.handleSaveClick}>保存</Button>
+                    <Button positive onClick={this.handleSaveClick}>
+                      保存
+                    </Button>
                   </Button.Group>
                 </div>
               </div>
             </div>
 
-            <Modal open={previewVisible} closeIcon onClose={()=>{this.setState({previewVisible: false})}}>
+            <Modal
+              open={previewVisible}
+              closeIcon
+              onClose={() => {
+                this.setState({ previewVisible: false });
+              }}
+            >
               <Modal.Content image>
-                <img style={{ width: '100%' }} src={previewImage} alt=''/>
+                <img style={{ width: '100%', height: '100%' }} src={previewImage} alt="" />
               </Modal.Content>
             </Modal>
           </Modal.Content>
         </Modal>
+        <Modal
+          open={true}
+          closeIcon
+          onClose={() => {
+            this.setState({ detailVisible: false });
+          }}
+          style={{ width: '642px', borderRadius: '8px', marginTop: '4rem', marginBottom: '4rem' }}
+        >
+          <Modal.Content>
+            <div className={styles.DetailWrapper} />
+          </Modal.Content>
+        </Modal>
       </div>
-    )
+    );
   }
 }
