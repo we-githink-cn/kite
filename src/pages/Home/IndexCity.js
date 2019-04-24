67e84899ac5c8f017a9aaf8e9324eee44ad04aa2
Maozk <549595297@qq.com>
2019-04-24 23:07:38 +0800

    同城页面

--- src/pages/Home/IndexCity.js
+++ src/pages/Home/IndexCity.js
@@ -24,11 +24,20 @@ export default class IndexCity extends Component {
         <div className={styles.CityHeaderWrapper} style={{ backgroundImage: `url(http://pic.vjshi.com/2017-01-20/1602e2b49097fc80fcf57247fa9bc1eb/00002.jpg?x-oss-process=style/watermark)`, }}>
           <div className={styles.CityHeaderInfo}>
             <div className={styles.CityHeaderName}>
-              济南
+              潍坊
             </div>
             <div className={styles.CityHeaderDesc}>
-              济南因境内泉水众多，拥有“七十二名泉”，被称为“泉城”，素有“四面荷花三面柳，一城山色半城湖”的美誉，济南八景闻名于世，是拥有“山、泉、湖、河、城”独特风貌的旅游城市，是国家历史文化名城、首批中国优秀旅游城市，史前文化——龙山文化的发祥地之一。
+              潍坊，山东省下辖地级市，古称“潍州”、“潍县”，又名“鸢都”，位于山东半岛的中部，与青岛、日照、淄博、烟台、临沂等地相邻。
+              气候属暖温带季风型半湿润大陆性气候，市域地貌自北向南，由低到高，形成几个台阶。截至2016年，辖4区6市2县。2017年总人口936.3万人。
             </div>
+          </div>
+        </div>
+        <div className={styles.CityContentWrapper}>
+          <div className={styles.CityMainWrapper}>
+
+          </div>
+          <div className={styles.CityMateWrapper}>
+
           </div>
         </div>
       </div>
